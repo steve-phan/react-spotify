15a80b01615c394eb49cdf1941c82088edf0b83a
vUnAmp <phanhaingoc@gmail.com>
2021-04-20 23:59:05 +0200

    reset all stuffs

--- src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  api: {
-    baseUrl: 'https://api.spotify.com/v1',
-    authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: 'cf52c00105d3495abe82bc37e3e931ea',
-    clientSecret: '66314e713b8641ccac6b986b9516d5a2',
-  },
-};
